7f5f59ddb9583439facd8e90929eb32dcdd13428
HeukGGu <101760091+Si-Woo91@users.noreply.github.com>
2022-05-09 19:58:10 +0900

    Add files via upload

---
+++ 1D1Q_backjoon/Day44.java
@@ -0,0 +1,45 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*문제	2292번
+
+위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 
+그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다. 
+숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오. 
+예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
+
+출력
+입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.*/
+
+public class Day44 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine()); // 숫자 입력
+		
+		int room = 1;		// 지나가는 방의 수
+		int min_num = 2;	// 동일 라인의 최소 번호
+		
+		if(N == 1) {
+			bw.write(room+"");
+		}else {
+			while(min_num <= N) {
+			min_num = min_num+(room*6);	// 다음 라인의 방 중 최소 번호 구하기
+			room++;	// 지나가는 방의 수 증가
+			}
+			bw.write(room+"");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
